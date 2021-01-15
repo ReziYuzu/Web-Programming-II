a0c9e8cc30951f50951b4eb0050ee31e2afbf63e
ReziYuzu <56716036+ReziYuzu@users.noreply.github.com>
2021-01-15 17:15:39 +0700

    Add files via upload

---
+++ Tugas/Pratikum/Pratikum 4/v_index.php
@@ -0,0 +1,12 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <p align='justify'>Pada pengertian codeignitier diatas tadi dijelaskan bahwa coseignitier menggunkan metode MVC. Apa itu MVC? Kita juga harus tahu apa itu MVC sebelum masuk dan lebih jauh dalam belajar codeignitier.</p>
+
+    <p>MVC adalah teknik konsep yang memisahkan konponen utama menjadi tiga komponen yaitu model, view dan controller.</p>
+    
+    <ol type='a'>
+
+    <li>Model</li>
+    <p align='justify'>Model adalah kelas yang merepresentasikan atau memodelkan tipe data yang akan digunakan oleh aplikasi.</p>
+    
+</section>
