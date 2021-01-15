98584c01fd2b179b9ae4f245d4fbbc19976d6080
ReziYuzu <56716036+ReziYuzu@users.noreply.github.com>
2021-01-15 17:21:18 +0700

    Add files via upload

---
+++ Tugas/pustaka_booking_5C/application/controllers/perkenalan.php
@@ -0,0 +1,13 @@
+<?php
+
+class Perkenalan extends CI_Controller
+{
+	
+	public function index()
+	{
+		echo "<h3> Perkenalan</h3>";
+		echo "Nama saya Solehudin Alfah Rezi 
+		saya tinggal di Tegal 
+		saya suka nonton Anime";
+	}
+}
